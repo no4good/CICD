0ee2e5093501074722dde816ad997f60dcb7d868
no4good <no4goodll@gmail.com>
2020-02-19 12:19:59 +0200

    add 0

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var express = require("express");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const mongoose = require("mongoose");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+mongoose
+  .connect(process.env.MongodbURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("DB connected"))
+  .catch(error => handleError(error));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
